550df508977a00fbe016e7fddb65ce83fa5c59fc
juyoungh98 <77319546+juyoungh98@users.noreply.github.com>
2023-09-09 19:38:46 +0900

    9/9

--- 9-8/11005.py
+++ 9-8/11005.py
@@ -5,13 +5,17 @@ ref = '0123456789' + string.ascii_uppercase
 n, b = map(int, input().split())
 
 count = 0
-while b**count < n:
+while b**count <= n:
     count += 1
+    # print("count =", count)
 
 result = ''
 for i in range(count-1, -1, -1):
     num = n//(b**i)
+    # print("num =", num)
     result += ref[num]
+    # print("result =", result)
     n -= num*(b**i)
+    # print("n =", n)
 
 print(result)
